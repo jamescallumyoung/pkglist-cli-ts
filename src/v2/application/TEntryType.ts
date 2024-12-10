405ae08047ca349809fa6eeca1c2e7a92045d556
James Young <hello@jamesyoung.ch>
2024-12-10 05:34:24 +0100

    feat: add type guard

--- src/v2/application/TEntryType.ts
+++ src/v2/application/TEntryType.ts
@@ -3,3 +3,7 @@ export const EntryTypes = [
 ] as const;
 
 export type TEntryType = (typeof EntryTypes)[number];
+
+export const isEntryType = (u: unknown): u is TEntryType =>
+    typeof u === "string" &&
+    EntryTypes.includes(u as TEntryType);
