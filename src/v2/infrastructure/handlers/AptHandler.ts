412434598bc979e2b42403e5008486bf25d3a892
James Young <hello@jamesyoung.ch>
2024-12-10 06:27:57 +0100

    feat: add installFns

--- src/v2/infrastructure/handlers/AptHandler.ts
+++ src/v2/infrastructure/handlers/AptHandler.ts
@@ -1,4 +1,5 @@
 import type {TInstallPackagesWithEntryTypeCommand} from "&/application/commands/TInstallPackagesWithEntryTypeCommand.js";
+import {installFns, PLACEHOLDER} from "&/infrastructure/utils/installFns.js";
 
 export class AptHandler {
     static async installAptPackages(command: TInstallPackagesWithEntryTypeCommand): Promise<void> {
@@ -24,10 +25,10 @@ export class AptHandler {
     }
 
     private static makeAptInstallCommand(packages: string[]): string {
-        return `sudo apt install -y `.concat(packages.join(" "));
+        return installFns['apt'].replace(PLACEHOLDER, packages.join(" "));
     }
 
     private static makeAddAptRepositoryCommand(packages: string[]): string {
-        return `sudo add-apt-repository -y `.concat(packages.join(" "));
+        return installFns['apt-repo'].replace(PLACEHOLDER, packages.join(" "));
     }
 }
