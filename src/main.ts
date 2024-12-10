3a71ab089e5d8c37d322710476479e232d737c16
James Young <hello@jamesyoung.ch>
2024-12-10 09:45:56 +0100

    feat: add cli wrapper

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
 import {__installationService} from "&/application/installationService.js";
 import {EntryTypeAdaptor} from "&/infrastructure/adaptors/EntryTypeAdaptor.js";
+import {cli} from "&/presentation/cli.js";
 import {__yargsExecCommandHandler} from "&/presentation/yargsExecCommandHandler.js";
 
-const cliSvcFn = __yargsExecCommandHandler(
+await cli(__yargsExecCommandHandler(
     __installationService(
         new EntryTypeAdaptor([ 'echo', 'apt', 'apt-repo', 'snap', 'snap-classic', 'flatpak' ]),
     ),
-);
-
-console.log(chalk.bgGreen.black.bold("PARSING JSON"));
-await cliSvcFn({ dry: true, file: "/Users/jamesyoung/repos/pkglist-cli-ts/content.pkglist.json" });
-
-console.log(chalk.bgGreen.black.bold("PARSING JSON5"));
-await cliSvcFn({ dry: true, file: "/Users/jamesyoung/repos/pkglist-cli-ts/content.pkglist.json5" });
-
-console.log(chalk.bgGreen.black.bold("PARSING YAML"));
-await cliSvcFn({ dry: true, file: "/Users/jamesyoung/repos/pkglist-cli-ts/content.pkglist.yaml" });
+));
--- src/v2/presentation/cli.ts
+++ src/v2/presentation/cli.ts
@@ -1,45 +1,32 @@
-// import {argv as processArgv} from "node:process";
-// import yargs from 'yargs';
-// import {hideBin} from 'yargs/helpers';
-// import {installationService} from "../application/installationService.js";
-//
-// export const cli = async (is: ReturnType<typeof installationService>): Promise<void> => {
-//     yargs(hideBin(processArgv))
-//         .command(
-//             'exec [file]',
-//             'Install the packages specified in a .pkglist.json file.',
-//             (yargs) => yargs
-//                 .positional('file', {
-//                     describe: 'Filepath to a .pkglist.json file. Or "-", to read from stdin.',
-//                     type: 'string',
-//                     default: '-',
-//                 })
-//                 // .option('dry', {
-//                 //     description: 'Execute a dry run and return the script that would have been executed.',
-//                 //     type: 'boolean',
-//                 //     default: false,
-//                 // })
-//                 // option: run in order
-//                 // option: run only selected targets
-//                 .demandOption(['prefix'])
-//                 .version(false),
-//             (args) => {
-//                 const {file} = args;
-//
-//                 // TODO
-//
-//                 is({
-//                     config: {},
-//                     entries: [{
-//                         type: 'echo',
-//                         package: 'foobar',
-//                     }],
-//                 });
-//             },
-//         )
-//         .strictCommands()
-//         .demandCommand()
-//         .help()
-//         .version(false)
-//         .parse();
-// }
+import {argv as processArgv} from "node:process";
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+import {__yargsExecCommandHandler} from "./yargsExecCommandHandler.js";
+
+export const cli = async (yargsExecCommandHandler: ReturnType<typeof __yargsExecCommandHandler>): Promise<void> => {
+    yargs(hideBin(processArgv))
+        .command(
+            'exec [file]',
+            'Install the packages specified in a .pkglist.json file.',
+            (yargs) => yargs
+                .positional('file', {
+                    describe: 'Filepath to a pkglist file. Or "-", to read from stdin.',
+                    type: 'string',
+                    default: '-',
+                })
+                .option('dry', {
+                    description: 'Execute a dry run and return the script that would have been executed.',
+                    type: 'boolean',
+                    default: false,
+                })
+                // option: run in order
+                // option: run only selected targets
+                .version(false),
+            (args) => yargsExecCommandHandler(args),
+        )
+        .strictCommands()
+        .demandCommand()
+        .help()
+        .version(false)
+        .parse();
+}
